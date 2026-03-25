60932ac376ecb9b27141de7a9a1a1ba37a24b50c
Manuell00 <manuell.caselli@gmail.com>
2026-03-25 17:49:19 +0100

    Shorten partner map CTA label

--- src/components/PartnerPageClient.tsx
+++ src/components/PartnerPageClient.tsx
@@ -304,7 +304,7 @@ function PartnerSection({
                         <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
                       </svg>
                     </span>
-                    {locationLabel === "Posizione" ? "Apri su Google Maps" : "Open in Google Maps"}
+                    {locationLabel === "Posizione" ? "Mappe" : "Maps"}
                   </a>
                 </div>
               </div>
