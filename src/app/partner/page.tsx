4d1a533eaf9e256a8350b405f422e30f96c3b877
Manuell00 <manuell.caselli@gmail.com>
2026-03-25 02:51:35 +0100

    Update San Bartolomeo featured logo image

--- src/app/partner/page.tsx
+++ src/app/partner/page.tsx
@@ -31,7 +31,7 @@ const marosaLightboxImages = [
 ];
 
 /* San Bartolomeo: property photos only in gallery, logo displayed separately */
-const sanBartolomeoLogo = "/images/partner/san-bartolomeo/casa-mare-logo.jpg";
+const sanBartolomeoLogo = "/images/partner/san-bartolomeo/sb-1.jpg";
 const sanBartolomeoImages = [
   "/images/partner/san-bartolomeo/sb-2.jpg",
   "/images/partner/san-bartolomeo/sb-3.jpg",
